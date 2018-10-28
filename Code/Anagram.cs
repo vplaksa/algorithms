51c9f3a4df7ad8464a26b85b498b4ee72180585c
unknown <vplaksa@gmail.com>
2018-10-28 21:43:03 +0200

    anagram

---
+++ Code/Anagram.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Code
+{
+    public class Anagram
+    {
+        public static bool IsAnagram(string s1, string s2)
+        {
+            var charArray1 = s1.ToCharArray();
+            var charArray2 = s2.ToCharArray();
+
+            Array.Sort(charArray1);
+            Array.Sort(charArray2);
+
+            return charArray1.SequenceEqual(charArray2); ;
+        }
+
+        public static bool IsAnagramBuffer(string s1, string s2)
+        {
+            var charArray1 = s1.ToCharArray();
+            var charArray2 = s2.ToCharArray();
+
+            if (charArray1.Length != charArray2.Length)
+                return false; //obvious
+
+            
+
+            // bool[] checkbuffer = new bool[charArray1.Length];
+
+            var r1 = Fill(charArray1, charArray2);//, checkbuffer);
+            var r2 = Fill(charArray2, charArray1);//, checkbuffer);
+
+            //такой учет не прокатывает
+
+
+            return true;
+        }
+
+        private static bool Fill(char[] a1, char[] a2)//, bool[] buf)
+        {
+            int l = a1.Length;
+            for (int i = 0; i < l; i++)
+            {
+                var current = a1[i];
+                var oneTrue = false;
+                for (int j = 0; j < l; j++)
+                {
+                    if (a2[j] == current)
+                    {
+                       // buf[i] = true;
+                        oneTrue = true;
+                    }
+                    //else
+                    //    buf[i] = false;
+                }
+                if (oneTrue == false)
+                {
+                   // buf[i] = false;
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ UnitTestProject1/Anagram.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class TestAnagram
+    {
+        [TestMethod]
+        public void IsAnagramTest()
+        {
+            var s1 = "abc";
+            var s2 = "cba";
+
+            AnagramAssert(s1, s2);
+        }
+
+        [TestMethod]
+        public void IsAnagramTest_Negative()
+        {
+            var s1 = "abcc";
+            var s2 = "cba";
+
+            AnagramAssert(s1, s2);
+        }
+
+        [TestMethod]
+        public void IsAnagramTest_Negative2()
+        {
+            var s1 = "тест";
+            var s2 = "естт";
+
+            AnagramAssert(s1, s2);
+        }
+
+        [TestMethod]
+        public void IsAnagramTest_Negative3()
+        {
+            var s1 = "тестт";
+            var s2 = "сеетт";
+
+            AnagramAssert(s1, s2);
+        }
+
+        private void AnagramAssert(string s1, string s2) => Assert.IsTrue(Code.Anagram.IsAnagramBuffer(s1, s2));
+    }
+}
